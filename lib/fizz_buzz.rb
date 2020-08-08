74b2870ffbc86874154ad0ba58981ee3c6b27a94
emiliano-ma <emilianomainero@gmail.com>
2020-08-08 08:30:38 +0200

    work finalized, refactoring done, I am so bad at commiting frecuently goshh!

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,35 @@
 def fizz_buzz(number)
-    number
+    if number.is_a?(String)
+        "#{number} is not a number, we don't divide other objects yet ;P"
+    elsif has_zero_reminder(number, 15)
+        'fizz buzz'
+    elsif has_zero_reminder(number, 5)
+        'buzz'
+    elsif has_zero_reminder(number, 3)
+        'fizz'
+    else
+        number
+    end
 end
+
+def has_zero_reminder (number, divider)
+    number % divider == 0
+end
+
+
+# def fizz_buzz(number)
+#     if number.is_a?(String)
+#         "#{number} is not a number, we don't divide other objects yet ;P"
+#     elsif number % 15 == 0
+#         'fizz buzz'
+#     elsif number % 5 == 0
+#         'buzz'
+#     elsif number % 3 == 0
+#         'fizz'
+#     else
+#         number
+#     end
+# end
+# puts "What's your number to Fizz Buzz check?"
+# your_num = gets.chomp.to_i
+# puts fizz_buzz(your_num)
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,4 +5,22 @@ describe 'fizz_buzz' do
     it 'returns 1 if number is 1' do
         expect(fizz_buzz(1)).to eq 1
     end
+    it "returns 'fizz' if number is divisible by 3" do
+        expect(fizz_buzz(3)).to eq 'fizz'
+    end
+    it "returns 'buzz' if number is  divisible by 5" do
+        expect(fizz_buzz(5)).to eq 'buzz'
+    end
+    it "returns 'fizz buzz' if number is divisible by 15" do
+        expect(fizz_buzz(15)).to eq 'fizz buzz'
+    end 
+    it "returns 1.298 if number is 1.298" do
+        expect(fizz_buzz(1.298)).to eq 1.298
+    end
+    it "returns -89.7 if number is -89.7" do
+        expect(fizz_buzz(-89.7)).to eq -89.7
+    end
+    it "returns 'dinosaurs is not a number, we don't divide other objects yet ;P' if number is 'dinosaurs'" do
+        expect(fizz_buzz('dinosaurs')).to match "dinosaurs is not a number, we don't divide other objects yet ;P"
+    end
 end
